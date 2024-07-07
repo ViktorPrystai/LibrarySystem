cdeeaf44c634bc51a9503e02d682c09c37d418d2
ViktorPrystai <viktor.prystai.21@pnu.edu.ua>
2024-07-07 22:56:27 +0300

    fixed register bug

--- library/authentication/models.py
+++ library/authentication/models.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 ROLE_CHOICES = (
@@ -67,9 +67,9 @@ class CustomUser(AbstractBaseUser):
         param is_active: user role, default value False
         type updated_at: bool
     """
-    first_name = models.CharField(max_length=20, default=None)
-    last_name = models.CharField(max_length=20, default=None)
-    middle_name = models.CharField(max_length=20, default=None)
+    first_name = models.CharField(max_length=20, default=None, null=True)
+    last_name = models.CharField(max_length=20, default=None, null=True)
+    middle_name = models.CharField(max_length=20, default=None, null=True)
     email = models.CharField(max_length=100, unique=True, default=None)
     password = models.CharField(default=None, max_length=255)
     created_at = models.DateTimeField(editable=False, auto_now=datetime.datetime.now())
@@ -77,6 +77,9 @@ class CustomUser(AbstractBaseUser):
     role = models.IntegerField(choices=ROLE_CHOICES, default=0)
     is_active = models.BooleanField(default=False)
     id = models.AutoField(primary_key=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     objects = CustomUserManager()
